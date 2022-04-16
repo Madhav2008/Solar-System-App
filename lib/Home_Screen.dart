8b6e7e5b3f083d990a218bc1d14d593a58edd441
arora <aroramadhav65@gmail.com>
2022-04-16 18:56:16 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -41,28 +41,28 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       textAlign: TextAlign.left,
                     ),
-                    DropdownButton(
-                      items: [
-                        DropdownMenuItem(
-                          child: Text(
-                            'Solar System',
-                            style: TextStyle(
-                              fontFamily: 'Avenir',
-                              fontSize: 24,
-                              color: const Color(0x7cdbf1ff),
-                              fontWeight: FontWeight.w500,
-                            ),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ],
-                      onChanged: (value) {},
-                      icon: Padding(
-                        padding: const EdgeInsets.only(left: 16.0),
-                        child: Image.asset('assets/drop_down_icon.png'),
-                      ),
-                      underline: SizedBox(),
-                    ),
+                    // DropdownButton(
+                    //   items: [
+                    //     DropdownMenuItem(
+                    //       child: Text(
+                    //         'Solar System',
+                    //         style: TextStyle(
+                    //           fontFamily: 'Avenir',
+                    //           fontSize: 24,
+                    //           color: const Color(0x7cdbf1ff),
+                    //           fontWeight: FontWeight.w500,
+                    //         ),
+                    //         textAlign: TextAlign.left,
+                    //       ),
+                    //     ),
+                    //   ],
+                    //   onChanged: (value) {},
+                    //   icon: Padding(
+                    //     padding: const EdgeInsets.only(left: 16.0),
+                    //     child: Image.asset('assets/drop_down_icon.png'),
+                    //   ),
+                    //   underline: SizedBox(),
+                    // ),
                   ],
                 ),
               ),
