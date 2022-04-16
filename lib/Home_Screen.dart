36c9fa611aa191da1319aa6fa3971b2e4c6cd004
arora <aroramadhav65@gmail.com>
2022-04-16 19:31:06 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, import_of_legacy_library_into_null_safe
+// ignore_for_file: file_names, import_of_legacy_library_into_null_safe, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:solar_system/Data.dart';
