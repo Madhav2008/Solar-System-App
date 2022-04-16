cc34114d0919a30e3ab94655bf42908b8554699d
arora <aroramadhav65@gmail.com>
2022-04-16 18:49:42 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -82,7 +82,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPage,
+                          Material,
                         );
                       },
                       child: Stack(
