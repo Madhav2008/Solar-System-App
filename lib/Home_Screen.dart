b229142e231e044434d14805c1f81bc9a02378de
arora <aroramadhav65@gmail.com>
2022-04-16 18:47:49 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -82,7 +82,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (builder,)=>DetailScreen(planetInfo: planets[index])),
+                          MaterialPageRoute(builder: (builder)=>DetailScreen(planetInfo: planets[inde])),
                         );
                       },
                       child: Stack(
