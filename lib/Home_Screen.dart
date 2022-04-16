2104813f9dd311f3c90f23d7b26f5e51bf2d04eb
arora <aroramadhav65@gmail.com>
2022-04-16 16:16:11 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-// import 'package:flutter_universe/detail_page.dart';
+import 'package:flutter_universe/detail_page.dart';
 import 'constants.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
